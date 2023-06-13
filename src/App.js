9cb7fd493d5c8a7c8e8172d425f371ea67bf4d51
F3riko <vyrydy@mail.ru>
2023-06-13 18:26:47 +0300

    Hooks for button, fixes to colors

--- src/App.js
+++ src/App.js
@@ -17,43 +17,43 @@ const App = () => {
         onClickFunction={handleClick}
         digit1="1"
         digit2="2"
-        className1="danger m-3"
-        className2="success m-3"
+        className1="danger"
+        className2="success"
       />
       <LineComponent
         onClickFunction={handleClick}
         digit1="2"
         digit2="4"
-        className1="danger m-3"
-        className2="success m-3"
+        className1="danger"
+        className2="success"
       />
       <LineComponent
         onClickFunction={handleClick}
         digit1="3"
         digit2="6"
-        className1="danger m-3"
-        className2="success m-3"
+        className1="danger"
+        className2="success"
       />
       <LineComponent
         onClickFunction={handleClick}
         digit1="4"
         digit2="8"
-        className1="danger m-3"
-        className2="success m-3"
+        className1="danger"
+        className2="success"
       />
       <LineComponent
         onClickFunction={handleClick}
         digit1="5"
         digit2="10"
-        className1="danger m-3"
-        className2="success m-3"
+        className1="danger"
+        className2="success"
       />
       <LineComponent
         onClickFunction={handleClick}
         digit1="6"
         digit2="12"
-        className1="danger m-3"
-        className2="success m-3"
+        className1="danger"
+        className2="success"
       />
       <AlertComponent digit={clicked} />
     </div>
--- src/components/button.js
+++ src/components/button.js
@@ -1,38 +1,18 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
-class ButtonComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      clicked: false,
-      className: `btn btn-outline-${props.className}`,
-    };
-  }
+const ButtonComponent = ({ digit, onClickFunction, className }) => {
+  const buttonClassName = `btn btn-outline-${className} m-3`
 
-  handleClick = () => {
-    const { digit, onClickFunction, className } = this.props;
+  const handleClick = () => {
     onClickFunction(digit);
-    this.state.clicked === false
-      ? this.setState({
-          clicked: true,
-          className: `btn btn-outline-${className}`,
-        })
-      : this.setState({
-          clicked: false,
-          className: `btn btn-${className}`,
-        });
   };
 
-  render() {
-    const { digit } = this.props;
-    const { className } = this.state;
-    return (
-      <button onClick={this.handleClick} className={className}>
-        {digit}
-      </button>
-    );
-  }
-}
+  return (
+    <button onClick={handleClick} className={buttonClassName}>
+      {digit}
+    </button>
+  );
+};
 
 export default ButtonComponent;
