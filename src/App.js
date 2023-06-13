8a6a27596b61801192bcc69055916389e5c30d73
F3riko <vyrydy@mail.ru>
2023-06-13 18:09:07 +0300

    Functional components and hooks in App

--- src/App.js
+++ src/App.js
@@ -4,66 +4,60 @@ import "bootstrap/dist/css/bootstrap.css";
 import LineComponent from "./components/line";
 import AlertComponent from "./components/alert";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      clicked: "nothing",
-    };
-  }
-  handleClick = (digit) => {
-    this.setState({ clicked: digit });
+const App = () => {
+  const [clicked, setClicked] = React.useState("nothing");
+
+  const handleClick = (digit) => {
+    setClicked(digit);
   };
 
-  render() {
-    return (
-      <div className="col-4 offset-1 center">
-        <LineComponent
-          onClickFunction={this.handleClick}
-          digit1="1"
-          digit2="2"
-          className1="danger m-3"
-          className2="success m-3"
-        />
-        <LineComponent
-          onClickFunction={this.handleClick}
-          digit1="2"
-          digit2="4"
-          className1="danger m-3"
-          className2="success m-3"
-        />
-        <LineComponent
-          onClickFunction={this.handleClick}
-          digit1="3"
-          digit2="6"
-          className1="danger m-3"
-          className2="success m-3"
-        />
-        <LineComponent
-          onClickFunction={this.handleClick}
-          digit1="4"
-          digit2="8"
-          className1="danger m-3"
-          className2="success m-3"
-        />
-        <LineComponent
-          onClickFunction={this.handleClick}
-          digit1="5"
-          digit2="10"
-          className1="danger m-3"
-          className2="success m-3"
-        />
-        <LineComponent
-          onClickFunction={this.handleClick}
-          digit1="6"
-          digit2="12"
-          className1="danger m-3"
-          className2="success m-3"
-        />
-        <AlertComponent digit={this.state.clicked} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="col-4 offset-1 center">
+      <LineComponent
+        onClickFunction={handleClick}
+        digit1="1"
+        digit2="2"
+        className1="danger m-3"
+        className2="success m-3"
+      />
+      <LineComponent
+        onClickFunction={handleClick}
+        digit1="2"
+        digit2="4"
+        className1="danger m-3"
+        className2="success m-3"
+      />
+      <LineComponent
+        onClickFunction={handleClick}
+        digit1="3"
+        digit2="6"
+        className1="danger m-3"
+        className2="success m-3"
+      />
+      <LineComponent
+        onClickFunction={handleClick}
+        digit1="4"
+        digit2="8"
+        className1="danger m-3"
+        className2="success m-3"
+      />
+      <LineComponent
+        onClickFunction={handleClick}
+        digit1="5"
+        digit2="10"
+        className1="danger m-3"
+        className2="success m-3"
+      />
+      <LineComponent
+        onClickFunction={handleClick}
+        digit1="6"
+        digit2="12"
+        className1="danger m-3"
+        className2="success m-3"
+      />
+      <AlertComponent digit={clicked} />
+    </div>
+  );
+};
 
 export default App;
